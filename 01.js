9cfffd9340d412b208cf4f02ed713f97e1c38ff6
AmanKumarWeb19 <akgupta0524@gmail.com>
2024-07-09 21:03:55 +0530

    using loop

---
+++ 01.js
@@ -0,0 +1,6 @@
+// Print "Hello World" to the console.
+
+// console.log("Hello World");
+
+let x = "Hello World"
+console.log(x)
---
+++ 02.js
@@ -0,0 +1,25 @@
+// Print the Numbers from 1 to 5 to the console using a loop
+
+// using For Loop:-
+
+// for(let i=1;i<=5;i++){
+//   console.log(i)
+// }
+
+
+// Using While Loop :-
+
+// let i=1
+// while(i<=5){
+//   console.log(i)
+//   i++
+// }
+
+
+// Using Do-While Loop:-
+
+let i=1
+do{
+  console.log(i)
+  i++
+}while(i<=5)
