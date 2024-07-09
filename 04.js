d0b2ec6eb3ba63904cbefea74c3da9074ede23f9
AmanKumarWeb19 <akgupta0524@gmail.com>
2024-07-09 21:12:10 +0530

    ar

---
+++ 04.js
@@ -0,0 +1,9 @@
+// Create a function to Calculate the area of a rectangle given its width and height
+
+
+function areaOfReactanle(length,width){
+  return length * width
+}
+
+let area = areaOfReactanle(5,8)
+console.log(area)
