787034043ad6e2c39f65fa6fb980c686a03eba0b
AmanKumarWeb19 <akgupta0524@gmail.com>
2024-07-10 14:56:07 +0530

    date and time

---
+++ 19.js
@@ -0,0 +1,8 @@
+/** Create a function that display the current date and time in a specific format */
+
+function getCurrentDateAndTime() {
+  let currentDate = new Date();
+  return currentDate.toLocaleString();
+}
+let date_time = getCurrentDateAndTime();
+console.log(date_time);
