ae65b4a0f82e50a2bba3b1059f864418d7630ba7
AmanKumarWeb19 <akgupta0524@gmail.com>
2024-07-10 14:48:29 +0530

    remove Occurance

---
+++ 17.js
@@ -0,0 +1,11 @@
+/** Find the largest element in an array using the reduce() method */
+
+function findLargestElement(arr) {
+  return arr.reduce((max, current) => (current > max ? current : max));
+}
+
+let arr = [1, 0, 8, 9, 75, 50, 34];
+
+let reduce = findLargestElement(arr);
+
+console.log(reduce);
---
+++ 18.js
@@ -0,0 +1,52 @@
+/** Remove All the occurances of a specific from an array */
+
+// USING FILTER METHOD:-
+// function removeOccuranceElement(arr, target) {
+//   return arr.filter((item) => item !== target);
+// }
+
+// let arr = [10, 10, 8, 5, 1, 6, 7];
+// let target = 10;
+// let removeoccur = removeOccuranceElement(arr, target);
+// console.log(removeoccur);
+
+// USING WITHOUT ANY METHOD :-
+
+// function removeOccuranceElement(arr, target) {
+//   let result = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     let shouldRemove = false;
+//     for (let j = 0; j < target.length; j++) {
+//       if (arr[i] === target[j]) {
+//         shouldRemove = true;
+//         break;
+//       }
+//     }
+//     if (!shouldRemove) {
+//       result.push(arr[i]);
+//     }
+//   }
+//   console.log(result);
+// }
+
+// let arr = [10, 10, 8, 7, 5, 1, 6, 7];
+// let target = [10, 7];
+// removeOccuranceElement(arr, target);
+
+// ..................................................................................
+
+/** if only one target is repeat then solve without any method */
+
+function removeOccurnace(arr, target) {
+  let result = [];
+  for(let i=0; i<arr.length; i++){
+    if(arr[i] !== target){
+      result.push(arr[i])
+    }
+  }
+  console.log(result)
+}
+
+let arr = [10, 10, 8, 5, 1, 6, 7];
+let target = 10
+removeOccurnace(arr,target)
