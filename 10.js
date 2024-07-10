e3e049be3366043abee8925a3dc32d22ce0557c8
AmanKumarWeb19 <akgupta0524@gmail.com>
2024-07-10 12:34:01 +0530

    basic Dom manipulation

---
+++ 10.js
@@ -0,0 +1,7 @@
+// Basic DOM Manipulation
+
+// Change the text of a paragraph on a webpage using Javascript.
+
+let para = document.getElementById("myPara");
+console.log(para);
+para.textContent = "this paragraph has been changed";
