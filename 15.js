bf0c7848aeec7da08a41efc9850ba3c2eb443e01
AmanKumarWeb19 <akgupta0524@gmail.com>
2024-07-10 13:49:19 +0530

    HOF for return only even number

---
+++ 15.js
@@ -0,0 +1,12 @@
+// Write a function that takes an array of numbers and
+// returns a new array with only the Even Numbers
+
+let arr = [12, 15, 20, 10, 40, 83, 24];
+
+function filterEvenNumber(arr) {
+  return arr.filter((num) => num % 2 === 0);
+}
+
+let x = filterEvenNumber(arr);
+console.log(x);
+console.log(arr);
