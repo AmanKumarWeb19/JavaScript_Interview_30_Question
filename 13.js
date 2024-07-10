0f602122b9f8a3e9655e4cb58f5bc9c687235159
AmanKumarWeb19 <akgupta0524@gmail.com>
2024-07-10 13:05:23 +0530

    factorial

---
+++ 13.js
@@ -0,0 +1,16 @@
+// Write a function that calculates the factorial of a given number
+//  factorial of 1 = always 1
+//  factorial of 0 = always 1
+
+const factorial = (num) => {
+  if (num === 0 || num === 1) {
+    return 1;
+  }else{
+    let fact = 1
+    for(let i=1; i<=num; i++){
+      fact *= i
+    }
+    console.log(fact)
+  }
+};
+factorial(5)
