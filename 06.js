2d4465057bb175b94da1f7242c8adbdc00818a48
Aman Kumar <akgupta0524@gmail.com>
2024-07-24 11:18:02 +0530

    even and odd

--- 06.js
+++ 06.js
@@ -1,12 +1,24 @@
-// Write a function that checks if a number is even or oddand
+// Write a function that checks if a number is even or odd and
 // returns "Even" or "Odd" accordingly.
 
-function EvenOrOdd(num) {
-  if (num % 2 == 0) {
-    return "Even";
-  } else {
-    return "Odd";
+// function EvenOrOdd(num) {
+//   if (num % 2 == 0) {
+//     return "Even";
+//   } else {
+//     return "Odd";
+//   }
+// }
+// let x = EvenOrOdd(2)
+// console.log(x)
+
+
+function EVENandODD(num){
+  if(num%2===0){
+    return "EVEN"
+  }else{
+    return "ODD"
   }
 }
-let x = EvenOrOdd(2)
+
+let x = EVENandODD(5)
 console.log(x)
