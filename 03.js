d3df22134866f45b0ae400483312c835cee08eb1
Aman Kumar <akgupta0524@gmail.com>
2024-07-24 10:54:17 +0530

    check sum

--- 03.js
+++ 03.js
@@ -1,8 +1,15 @@
 // Write a function to add two Numbers and return the results
 
-function addTwoNumbers(num1,num2){
-  return num1 + num2
-}
+// function addTwoNumbers(num1,num2){
+//   return num1 + num2
+// }
 
-let sum = addTwoNumbers(5,10)
-console.log(sum)
+// let sum = addTwoNumbers(5,10)
+// console.log(sum)
+
+
+function addNumber(sum1,sum2){
+  return sum1 + sum2
+}
+let x= addNumber(5,7)
+console.log(x)
