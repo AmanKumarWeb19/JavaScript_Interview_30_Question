786e13e3a9cd59aebe51123233fa578722778054
Aman Kumar <akgupta0524@gmail.com>
2024-07-24 11:41:00 +0530

    add num

--- 08.js
+++ 08.js
@@ -1,10 +1,23 @@
 // Find the sum of all the elements in an Array. 
 
+// let arr = [20,10,40,50]
+
+// let sum = 0
+
+// for(let i=0; i<arr.length; i++){
+//   sum += arr[i]
+// }
+// console.log(sum)
+
+
 let arr = [20,10,40,50]
 
-let sum = 0
 
-for(let i=0; i<arr.length; i++){
-  sum += arr[i]
+function FindSum(arr){
+  let sum=0
+  for(let i=0;i<arr.length;i++){
+    sum += arr[i]
+  }
+  console.log(sum)
 }
-console.log(sum)
+FindSum(arr)
