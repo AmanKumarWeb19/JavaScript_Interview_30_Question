804c65bc4a21a2130bd1b0fb5e5beafde56f0667
Aman Kumar <akgupta0524@gmail.com>
2024-07-24 11:38:02 +0530

    leap year

--- 07.js
+++ 07.js
@@ -1,10 +1,20 @@
 // Create a program that checks if a given year is a leap Year.
 
-const LeapYearCheck = (year) => {
-  if ((year % 4 == 0 && year % 100 !== 0) || year % 400 == 0) {
-    console.log("Yes It is a Leap Year");
+// const LeapYearCheck = (year) => {
+//   if ((year % 4 == 0 && year % 100 !== 0) || year % 400 == 0) {
+//     console.log("Yes It is a Leap Year");
+//   } else {
+//     console.log("No It is not a leap year");
+//   }
+// };
+// LeapYearCheck(1900);
+
+function leapYearCheck(year) {
+  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    return true;
   } else {
-    console.log("No It is not a leap year");
+    return false;
   }
-};
-LeapYearCheck(1900);
+}
+let x = leapYearCheck(3000)
+console.log(x)
