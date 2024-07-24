59d051110ff7c75d0d1172ccd89fc1a2a4d48f4f
Aman Kumar <akgupta0524@gmail.com>
2024-07-24 11:54:33 +0530

    basic dom

--- 09.js
+++ 09.js
@@ -1,15 +1,26 @@
 // Create an Object representing a car with properties like make, model, and year.
 // Add a method to the car object to start the Engine. 
 
+// let car={
+//   make:"Mahindra",
+//   model:"Scorpio",
+//   year:2022
+// }
+// // console.log(car)
+
+// car.startEngine = function(){
+//   console.log("Engine Started")
+// }
+
+// car.startEngine()
+
 let car={
   make:"Mahindra",
-  model:"Scorpio",
-  year:2022
+  model:"SCORPIO",
+  year:2024
 }
-// console.log(car)
-
-car.startEngine = function(){
-  console.log("Engine Started")
+car.startEngine=function(){
+  console.log(this.make+ " " +"Engine Started in" + " " + this.year)
 }
 
 car.startEngine()
--- 10.js
+++ 10.js
@@ -2,6 +2,10 @@
 
 // Change the text of a paragraph on a webpage using Javascript.
 
-let para = document.getElementById("myPara");
-console.log(para);
-para.textContent = "this paragraph has been changed";
+// let para = document.getElementById("myPara");
+// console.log(para);
+// para.textContent = "this paragraph has been changed";
+
+
+let para = document.getElementById("myPara")
+para.textContent ="paragraph has been changed"
